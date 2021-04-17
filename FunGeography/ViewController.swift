af3e70789f3e3754657d99c80f19a60d2cd41cb6
artazidele <77989249+artazidele@users.noreply.github.com>
2021-04-17 18:10:58 +0300

    Initial Commit

---
+++ FunGeography/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunGeography
+//
+//  Created by arta.zidele on 17/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
